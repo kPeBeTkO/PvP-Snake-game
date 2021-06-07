48108a9181356a4da6d945f621d00e23f68276a5
Lera Glebova <lera.glebova.2001@mail.ru>
2021-06-08 02:27:30 +0500

    textures

--- scr/SnakeGame/StartForm.Designer.cs
+++ scr/SnakeGame/StartForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace SnakeGame
 {
-    partial class Form1
+    partial class StartForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -29,10 +29,19 @@ namespace SnakeGame
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(778, 450);
+            this.Name = "StartForm";
+            this.Text = "PvPSnake";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
---
+++ scr/SnakeGame/StartForm.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeGame
+{
+    public partial class StartForm : Form
+    {
+        private Button start;
+        private Label text;
+        public StartForm(int fieldHeight, int fieldWidth)
+        {
+            Icon = new Icon("Textures\\icon.ico");
+            //BackColor = Color.Green;
+
+            start = new Button()
+            {
+                Text = "PLAY",
+                Location = new Point(Width / 2, Height / 2),
+                Size = new Size(Width / 4, Height / 6),
+                Font = new Font("impact", 50),
+                ForeColor = Color.White,
+                FlatStyle = FlatStyle.Flat,
+                BackColor = Color.FromArgb(0, 100, 100, 100),
+                
+            };
+
+            text = new Label()
+            {
+                //BackColor = Color.Green,
+                Location = new Point(Width / 2, Height / 4),
+                Text = "PvP Snake",
+                Font = new Font("impact", 70),
+                Size = new Size(Width / 5, Height / 8),
+                ForeColor = Color.White,
+                BackColor = Color.FromArgb(0, 0, 0, 0)
+        };
+
+            start.FlatAppearance.BorderSize = 0;
+            start.Click += (sender, args) =>
+            {
+                Application.Run(new GameForm(20, 20));
+            };
+            Controls.Add(start);
+            Controls.Add(text);
+            Brush green = new SolidBrush(Color.FromArgb(41, 135, 72));
+            Brush lime = new SolidBrush(Color.FromArgb(92, 188, 90));
+            Paint += (s, a) => 
+            {
+                var g = a.Graphics;
+                var y = 0;
+                var color = true;
+                var color2 = false;
+                while (y < Height)
+                {
+                    for (int i = 0; i < 12; i++)
+                    {
+                        if (color)
+                            g.FillRectangle(lime, new Rectangle(Width / 12 * i, y, Width / 12, Width / 12));
+                        else
+                            g.FillRectangle(green, new Rectangle(Width / 12 * i, y, Width / 12, Width / 12));
+                        color = !color;
+                    }
+                    color = color2;
+                    color2 = !color;
+                    y = y + Width / 12;
+                }
+                start.Size = new Size(Width / 4 , Height / 6);
+                start.Location = new Point(Width / 2 - start.Size.Width/2, Height / 2 - start.Size.Height / 2);
+                text.Size = new Size(Width / 2, Height / 6);
+                text.Location = new Point(Width / 2 - start.Size.Width / 2, Height / 4 - start.Size.Height / 2);
+                Controls.Add(start);
+                Controls.Add(text);
+            };
+            var timer = new Timer();
+            timer.Interval = 100;
+            timer.Enabled = true;
+            timer.Tick += (s, a) => Invalidate();
+            timer.Start();
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Icon = new Icon("Textures/icon.ico");
+            Text = "PvPSnake";
+        }
+    }
+}
